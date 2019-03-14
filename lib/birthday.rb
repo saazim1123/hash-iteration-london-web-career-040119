6ecea67a8433d4061364399b5436e391645c30e0
Adnan Azim <saazim1123@gmail.com>
2019-03-14 03:35:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,9 +7,9 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name, age|
-    if age <= 12
+    
       puts "Happy birthday to #{name}! You are now #{age} years old!"
-    end
+    
   end
 end
 
