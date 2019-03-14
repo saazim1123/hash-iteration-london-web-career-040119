34b0018d609803e6e096562d0af481d9bd96881d
Adnan Azim <saazim1123@gmail.com>
2019-03-14 03:31:33 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -17,11 +17,3 @@ def select_winner(passengers)
   winner
 end
 
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
-
-winner
