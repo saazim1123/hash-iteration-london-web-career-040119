8f13e0b7f190204cf3d14ceea83c04c4113c979c
Adnan Azim <saazim1123@gmail.com>
2019-03-14 03:29:14 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,6 +10,6 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
   select_winner.each do |suite, name|
-    if suite == :suite_a && 
+    if suite == :suite_a && name.start
   end
 end
